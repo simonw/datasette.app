2d8b78744da64b9a639255cd58a13d5f3ebd2c04
Cody Wilson <cody@codywilson.co>
2024-10-30 14:06:31 -0400

    remove obsolete check to delete test folder

--- download-python.sh
+++ download-python.sh
@@ -11,11 +11,3 @@ fi
 filename="cpython-${cpython_version}+${release_date}-${arch}-apple-darwin-install_only.tar.gz"
 
 standalone_python="python/"
-
-if [ ! -d "$standalone_python" ]; then
-    wget https://github.com/indygreg/python-build-standalone/releases/download/${release_date}/${filename}
-    tar -xzvf ${filename}                                                                          
-    rm -rf ${filename}
-    # Now delete the test/ folder, saving about 23MB of disk space
-    rm -rf python/lib/python3.9/test
-fi
