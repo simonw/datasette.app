151bd735767b3274df313c23970c8d801438e4b7
Cody Wilson <cody@codywilson.co>
2024-10-30 14:05:43 -0400

    add support for downloading native python for the system arch

--- download-python.sh
+++ download-python.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-release_date="20210724"
-filename="cpython-3.9.6-x86_64-apple-darwin-install_only-20210724T1424.tar.gz"
+release_date="20241016"
+cpython_version="3.12.7"
+
+arch=`uname -m`
+# Convert arch to aarch64 to match the build artifact name
+if [ $arch = "arm64" ]; then
+    arch="aarch64"
+fi
+
+filename="cpython-${cpython_version}+${release_date}-${arch}-apple-darwin-install_only.tar.gz"
 
 standalone_python="python/"
 
